1a923b56e3c54e5c027aca688720f6b83ccad83d
chuzhong <352995096@qq.com>
2021-05-29 10:22:08 +0800

    use ElementUI

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="hello">
+    <el-button type="success" @click="onClick1">成功按钮</el-button>
+    <el-button type="fail" @click="onClick2">失败按钮</el-button>
     <p>这是首页</p>
   </div>
 </template>
@@ -8,6 +10,24 @@
 export default {
   props: {
     msg: String
+  },
+  methods: {
+    onClick1() {
+      this.$message({
+        message: "这是一条消息"
+      })
+    },
+    onClick2() {
+      this.$alert('这是内容', '标题', {
+        confirmButtonText: "试试",
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${action}`
+          })
+        }
+      })
+    }
   }
 }
 </script>
